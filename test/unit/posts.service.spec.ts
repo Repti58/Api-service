ec29d1bf1080a174041e32ae82e57d1bfca0eece
repti85 <ids-reptile@bk.ru>
2023-10-14 21:23:22 +0300

    add readme

--- test/unit/posts.service.spec.ts
+++ test/unit/posts.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PostsService } from './posts.service';
+import { PostsService } from '../../src/posts/posts.service';
 import { HttpService } from '@nestjs/axios';
 import { of, throwError } from 'rxjs';
 
